adfe9476d3f405c9580e1a958e9e4d577d2fa172
Christian Lura <christian.lura@nrk.no>
2017-05-26 12:20:42 +0200

    Fikset Venstre-søk

--- valg.R
+++ valg.R
@@ -3,7 +3,7 @@ library(xlsx)
 library(zoo)
 library(data.table)
 
-år <- c(1945,1949,1953,1957,1961,1965,1969,1973,1977,1981,1985,1989,1993,1997,2001,2005,2009,2013)
+aar <- c(1945,1949,1953,1957,1961,1965,1969,1973,1977,1981,1985,1989,1993,1997,2001,2005,2009,2013)
 alle <- read.csv("data/alle_stemmer.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8")
 tot <- read.csv("data/res%.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8")
 
@@ -33,34 +33,38 @@ get.res <- function(år,kom,par) {
   data <- les.csv(år)
   data <- as.data.table(data)
   data <- data[kommune %like% kom & parti %like% par]
-  return(data)
+  
+  ##Ikke få med SV hvis det er Venstre vi er ute etter
+  venstre <- data[kommune %like% kom & parti == "Venstre"]
+  ifelse (par=="Venstre", return(venstre), return(data))
 }
+
 
 ## Hent resultat og prosentvis oppslutning fra en kommune i et gitt år. 
 get.oppsl <- function(år, kom, par){
   alle.res <- get.res(år,kom, "")
   total <- sum(alle.res[,3])
-  res <- alle.res[parti %like% par]
+  res <- get.res(år,kom,par)
   oppsl <- round(100*res[,3]/total, digits=1)
   names(oppsl) <- "%"
   res <- cbind(res,oppsl)
   return(res)
 }
 
-## Hent alle resultater for et partii en gitt kommune
+## Hent alle resultater for et parti i en gitt kommune
 get.alle.år <- function(kom, par) {
   alle.år <- sapply(aar, function(x){get.oppsl(x,kom,par)})
-  år <- as.data.frame(år)
+  aar <- as.data.frame(aar)
   alle.år <- t(as.data.frame(alle.år))
   alle.år <- as.data.table(alle.år)
-  alle.år <- cbind(år, alle.år)
+  alle.år <- cbind(aar, alle.år)
   return(alle.år)
 }
 
 ## Sjekk oppslutning mot landsresultat
 styrke <- function(år, kom, par){
   res <- get.oppsl(år,kom,par)
-  oppsl <- res[4]
+  oppsl <- res[,4]
   tot <- as.data.table(tot)
   land <- tot[parti %like% par]
   land <- land[1]
